3629063da017f021367408a40bceeee5f6bacaf4
Colin Unger <lockshaw@lockshaw.net>
2026-06-30 13:11:41 -0700

    Fix incorrect proj flake uri

--- flake.nix
+++ flake.nix
@@ -18,8 +18,7 @@
     flake-utils.url = "github:numtide/flake-utils";
 
     proj-repo = {
-      # url = "git+https://git.sr.ht/~lockshaw/proj";
-      url = "git+file:///home/lockshaw/x/ff/proj/proj";
+      url = "git+https://git.sr.ht/~lockshaw/proj";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
